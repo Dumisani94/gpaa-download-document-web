8316c4da1b19937973ec6f8127332d8dca56f948
Dumisani Ndhlovu <Dumisani.Ndhlovu@gpaa.gov.za>
2022-01-24 21:45:15 +0200

    Updated the JS fle

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/better-life-web'));
+app.use(express.static(__dirname + '/dist/gpaa-download-document-web'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
